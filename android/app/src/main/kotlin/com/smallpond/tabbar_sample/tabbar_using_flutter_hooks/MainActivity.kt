749e3d356389fd1f20fce80875c4e5b0e3062a29
Kosuke Izumi <ko-izumi@kddi.com>
2021-09-12 01:19:56 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/smallpond/tabbar_sample/tabbar_using_flutter_hooks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.smallpond.tabbar_sample.tabbar_using_flutter_hooks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('TabBar using flutter_hooks'),
+        ),
+        body: TabBarViewWidget(),
+      ),
+    );
+  }
+}
+
+class TabBarViewWidget extends HookWidget {
+  TabBarViewWidget({Key? key}) : super(key: key);
+
+  final List<Widget> tabList = [
+    const Tab(child: Text('Tab 1')),
+    const Tab(child: Text('Tab 2')),
+    const Tab(child: Text('Tab 3')),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final _controller = useTabController(initialLength: tabList.length);
+    return Column(
+      children: [
+        TabBar(
+          controller: _controller,
+          tabs: tabList,
+          labelColor: Theme.of(context).primaryColor,
+        ),
+        Expanded(
+          child: TabBarView(
+            controller: _controller,
+            children: const [
+              Center(child: Text('Tab 1 View')),
+              Center(child: Text('Tab 2 View')),
+              Center(child: Text('Tab 3 View')),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
